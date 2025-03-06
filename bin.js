d1cee565d23aebf63cadf1b2c0a99f9b8eb42add
GiacomoP99 <giacomo.01@hotmail.com>
2025-03-06 16:56:15 +0100

    can add children at any level

--- bin.js
+++ bin.js
@@ -11,21 +11,31 @@ const createInterface = () => {
   });
 };
 
-const INDEX_ROUTE = (
-  name,
-  joinedName,
-  path,
-  isIndex,
-  policy
-) => `const ${name} = React.lazy(() => import('@pages/${joinedName}'));
-
-export const route_${name} =  {
-    path: '${path}',
+const INDEX_ROUTE = (name, path, isIndex, policy) => `import React from 'react'
+const ${name} = React.lazy(() => import('@pages/${name}'));
+// OBJECT
+const Mapper = {
+  ${name}: <${name} />
+  // ADD MAPPER
+};
+// OBJECT
+const base = {
+    path: "${path}",
     isIndex: ${isIndex},
-    component: <${name} />,
-    children: null,
+    component: "${name}",
+    children: [],
     policy: ${policy || null}
 }
+// OBJECT
+const transform = obj => {
+  return {
+    ...obj,
+    component: Mapper[base.component],
+    children: obj.children?.map(el => transform(el))
+  };
+};
+
+export const route_${name} = transform(base);
 `;
 
 const askQuestion = (rl, question) => {
@@ -36,6 +46,21 @@ const askQuestion = (rl, question) => {
   });
 };
 
+const addInCorrect = (obj, parents, toAdd, index) => {
+  if (parents.length === index + 1 && obj.component === parents[index]) {
+    return {
+      ...obj,
+      children: [...obj.children, toAdd]
+    };
+  }
+  return {
+    ...obj,
+    children: obj.children.map(child =>
+      addInCorrect(child, parents, toAdd, index + 1)
+    )
+  };
+};
+
 const main = async () => {
   if (yargs.argv.init) {
     fs.mkdirSync('src/routes', { recursive: true }, err => {
@@ -44,55 +69,121 @@ const main = async () => {
     fs.cpSync('init', 'src/routes', { recursive: true });
   } else if (yargs.argv.info) {
     console.log(
-      `init - to create folders and index file\nadd - to create a new route`
+      `init - to create folders and index file\nadd - to create a new route`,
+      yargs.argv
     );
   } else {
     const rl = createInterface();
-    const name = await askQuestion(rl, 'Route name: ');
-    const policy = await askQuestion(rl, 'Policy name: ');
-    const isIndex = await askQuestion(rl, 'Is index (y/n): ').then(
-      data => data.toLowerCase() === 'y'
-    );
-    const path = await askQuestion(rl, 'Path: ');
-    fs.mkdirSync(`src/routes/${name}`, { recursive: true }, err => {
-      if (err) throw err;
-    });
-    fs.writeFile(
-      `src/routes/${name}/index.tsx`,
-      INDEX_ROUTE(name, name, path, isIndex, policy),
-      err => {
-        // In case of a error throw err.
-        if (err) throw err;
-      }
-    );
-    fs.readFile('src/routes/routes.tsx', 'utf-8', (err, contents) => {
-      if (err) {
-        return console.error(err);
-      }
-      const updated = contents
-        .replace(
-          '// DO NOT REMOVE ~IMPORT~',
-          `import { route_${name} } from './${name}';\n// DO NOT REMOVE ~IMPORT~`
-        )
-        .replace(
-          '// DO NOT REMOVE ~INTERNAL~',
-          `, route_${name}\n// DO NOT REMOVE ~INTERNAL~`
+    let tmp;
+    const parents = [];
+    if (yargs.argv.sub) {
+      do {
+        tmp = await askQuestion(rl, 'Parent name: ');
+        tmp && parents.push(tmp);
+      } while (tmp);
+    }
+    const name = yargs.argv.name
+      ? yargs.argv.name
+      : await askQuestion(rl, 'Route name: ');
+    const policy = yargs.argv.fast
+      ? null
+      : await askQuestion(rl, 'Policy name: ');
+    const isIndex = yargs.argv.fast
+      ? false
+      : await askQuestion(rl, 'Is index (y/n): ').then(
+          data => data.toLowerCase() === 'y'
         );
-      fs.writeFile('src/routes/routes.tsx', updated, 'utf-8', err2 => {
-        if (err2) {
-          console.log(err2);
+    const path = yargs.argv.fast
+      ? `/${name.toLowerCase()}`
+      : await askQuestion(rl, 'Path: ');
+    if (parents.length > 0) {
+      fs.readFile(
+        `src/routes/${parents[0]}/index.tsx`,
+        'utf-8',
+        (err, contents) => {
+          if (err) {
+            return console.error(err);
+          }
+          const [imports, mapper, object, result] = contents.split('// OBJECT');
+          const newImports = [
+            ...imports.split('\n'),
+            `const ${name} = React.lazy(() => import('@pages/${parents.join('/')}/${name}'));\n`
+          ].filter(el => !!el);
+          const newMapper = mapper.replace(
+            '// ADD MAPPER',
+            `,${name}: <${name} />\n// ADD MAPPER`
+          );
+          const [start, end] = object.split(' = ');
+          const parsedObj = JSON.parse(
+            end
+              .replace(';', '')
+              .replace(/(\w+):/g, '"$1":')
+              .replaceAll(`'`, '"')
+          );
+          const newObj = addInCorrect(
+            parsedObj,
+            parents,
+            {
+              path: path,
+              isIndex: isIndex,
+              component: name,
+              children: [],
+              policy: policy || null
+            },
+            0
+          );
+
+          fs.writeFile(
+            `src/routes/${parents[0]}/index.tsx`,
+            [
+              newImports.join('\n'),
+              newMapper,
+              [start, JSON.stringify(newObj)].join(' = '),
+              result
+            ].join('// OBJECT'),
+            'utf-8',
+            err2 => {
+              if (err2) {
+                console.log(err2);
+              }
+            }
+          );
         }
+      );
+    } else {
+      fs.mkdirSync(`src/routes/${name}`, { recursive: true }, err => {
+        if (err) throw err;
       });
-    });
+      fs.writeFile(
+        `src/routes/${name}/index.tsx`,
+        INDEX_ROUTE(name, path, isIndex, policy),
+        err => {
+          // In case of a error throw err.
+          if (err) throw err;
+        }
+      );
+      fs.readFile('src/routes/routes.tsx', 'utf-8', (err, contents) => {
+        if (err) {
+          return console.error(err);
+        }
+        const updated = contents
+          .replace(
+            '// DO NOT REMOVE ~IMPORT~',
+            `import { route_${name} } from './${name}';\n// DO NOT REMOVE ~IMPORT~`
+          )
+          .replace(
+            '// DO NOT REMOVE ~INTERNAL~',
+            `, route_${name}\n// DO NOT REMOVE ~INTERNAL~`
+          );
+        fs.writeFile('src/routes/routes.tsx', updated, 'utf-8', err2 => {
+          if (err2) {
+            console.log(err2);
+          }
+        });
+      });
+    }
     rl.close();
   }
 };
 
 main().catch(console.error);
-
-// let data = "import { Route } from txt";
-
-// fs.writeFile("src/routes/index.ts", data, (err) => {
-//   // In case of a error throw err.
-//   if (err) throw err;
-// });
